79f0892414ed9bff2061d39888fd813b56e4db7f
tiagot1 <tiagotorres.dev@gmail.com>
2022-09-18 23:49:36 -0300

    feito

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 const cotacaoBtn = [5.12, 4.8, 4.6, 5.2, 3.5, 5.1];
 const maiorPreco = Math.max.apply(null, cotacaoBtn);
 const menorPreco = Math.min.apply(null, cotacaoBtn);
-console.log(menorPreco);
-for (i = 0; i < cotacaoBtn.length; i++) {
-  const maior = maiorPreco;
-  const menor = menorPreco;
-  const idxMaior = cotacaoBtn.indexOf(maior);
-  const idxMenor = cotacaoBtn.indexOf(maior);
+const tenhoBtc = menorPreco;
+const maior = maiorPreco;
+const menor = menorPreco;
+const idxMaior = cotacaoBtn.indexOf(maior);
+const idxMenor = cotacaoBtn.indexOf(menor);
+console.log(
+  'O melhor dia da semana para comprar Bitcoin é o dia: ' +
+    (idxMenor + 1) +
+    ' valor: ' +
+    menorPreco,
+);
 
-  if (maior === maiorPreco) {
+if (tenhoBtc !== false) {
+  function venderBtc() {
+    const num = cotacaoBtn.slice(idxMenor);
+    const vender = num.reduce((acc, att) => {
+      return acc > att ? acc : att;
+    }, 0);
+    const lucro = vender - menorPreco;
     console.log(
-      'O melhor dia para comprar Bitcoin é o dia ' +
-        (idxMenor + 2) +
-        ' valor: ' +
-        menorPreco,
-    );
-    console.log(
-      'O melhor dia para vender Bitcoin é o dia ' +
+      'O melhor dia da semana para vender Bitcoin é o dia: ' +
         (idxMaior + 1) +
         ' valor: ' +
-        maiorPreco,
+        vender +
+        ' ' +
+        lucro.toFixed(2),
     );
-    break;
+    console.log(num);
   }
+  venderBtc();
+} else {
+  console.log('Você precisa comprar Bitcoin antes de vender.');
 }
